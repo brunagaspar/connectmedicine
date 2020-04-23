ba13c036bb1ef10e28e7566be2f979ef9b437279
José Lucas Pereira <jose.pereira@2s.com.br>
2020-04-23 12:13:18 -0300

    Atualização do requirements

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # Ponha aqui os dados do seu robozinho
-bottoken=''
+bottoken='ZmMwYmVmOTktZmI4OS00OTJiLWEwMzgtODViMTdhZGI3Zjg5OTk1ZGFlZGEtZDdh_PF84_a892a6ed-b823-46b8-8336-847d8f4722ae'
 #exemplo='Yia0dsfdsfsdf...' - tirado do site developer.webex.com
-botmail=""
+botmail="CONNECTEDMEDICINE@webex.bot"
 #exemplo='mybotmail@spark.io' - tirado do site developer.webex.com
 
 # Ponha aqui os dados do seu Webhook
 # webhook_url o endereco publico onde esta o app
-webhook_url=""
+webhook_url="https://botconnectedmed.herokuapp.com/"
 # exemplo webhook_url="https://mywebhook.com"
 # webhook_name e' o nome do gatilho que o Webex Teams gera para seu aplicativo entender
-webhook_name=""
+webhook_name="botconnectedmed"
 # exemplo webhook_name="meuprimeirochatbot"
